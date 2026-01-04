7fd2a9dab194b9a719ab2b10dea707a2402482a5
reevabajaj11 <reevabajaj75@gmail.com>
2026-01-04 17:27:49 +0530

    Updated images after feedback

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -47,8 +47,8 @@ export const teamSections = [
       { 
         name: "Avwal Kaur", 
         role: "Research Lead", 
-        image: "/team/Avwal_Kaur.jpg",
-        branch: "CSE-AI",
+        image: "/team/Avwal_Kaur.jpeg",
+        branch: "CSE",
         linkedin: "https://www.linkedin.com/in/avwal-kaur/",
         description: "Trying to become better than yesterday..."
       },
@@ -56,7 +56,7 @@ export const teamSections = [
         name: "Anjali Gupta", 
         role: "Research Lead", 
         image: "/team/Anjali_Gupta.jpeg",
-        branch: "CSE-AI",
+        branch: "ECE-AI",
         linkedin: "https://www.linkedin.com/in/anjali-gupta-7985b2288?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         description: "Curious, caffeinated, and trying my best.  "
       },
@@ -71,8 +71,8 @@ export const teamSections = [
       { 
         name: "Diya Gahlot", 
         role: "Media & PR Lead", 
-        image: "/team/Diya_Gahlot.jpg",
-        branch: "CSE-AI",
+        image: "/team/Diya_Gahlot.jpeg",
+        branch: "CSE",
         linkedin: "https://www.linkedin.com/",
         description: "Curating our brand voice and expanding outreach."
       },
@@ -99,7 +99,7 @@ export const teamSections = [
     id: "research",
     title: "Research Team",
     members: [
-       { name: "Anya Goyal", role: "Associate", image: "/team/Anya_Goyal.HEIC" },
+       { name: "Anya Goyal", role: "Associate", image: "/team/Anya_Goyal.jpeg" },
        { name: "Tanishka Arora", role: "Associate", image: "/team/Tanishka_Arora.jpg" },
        { name: "Avanie Sharma", role: "Head Coordinator", image: "/team/Avanie_Sharma.jpg" },
        { name: "Vaibhavi Srivastava", role: "Head Coordinator", image: "/team/Vaibhavi_Srivastava.jpg" },
@@ -117,22 +117,24 @@ export const teamSections = [
     id: "management",
     title: "Management Team",
     members: [
-      { name: "Ananya Pal", role: "Associate", image: "/team/Ananya_Pal.HEIC" },
+      { name: "Ananya Pal", role: "Associate", image: "/team/Ananya_Pal.jpeg" },
       { name: "Ariza Wasim", role: "Associate", image: "/team/Ariza_Wazim.jpg" },
       { name: "Mehak Devgan", role: "Associate", image: "/team/Mehak_Devgan.jpg" },
-      { name: "Ashima Kaushik", role: "Head Coordinator", image: "/team/Ashima Kaushik.jpg" },
-      { name: "Shalu Kumari", role: "Head Coordinator", image: "/team/Shalu_Kumari.jpg" },
+      { name: "Anjali Singh", role: "Head Coordinator", image: "/team/Anjali_Singh.jpg" },
+      { name: "Ashima Kaushik", role: "Head Coordinator", image: "/team/Ashima_Kaushik.jpg" },
       { name: "Navya", role: "Head Coordinator", image: "/team/Navya.jpg" },
+      { name: "Shalu Kumari", role: "Head Coordinator", image: "/team/Shalu_Kumari.jpg" },
       { name: "Akshita", role: "Coordinator", image: "/team/Akshita.jpg" },
-      { name: "Angel Gautam", role: "Coordinator", image: "/team/Angel_Gauatm.jpg" },
+      { name: "Angel Gautam", role: "Coordinator", image: "/team/Angel_Gautam.jpeg" },
       { name: "Khushi Kumari", role: "Coordinator", image: "/team/Khushi_Kumari.jpg" },
       { name: "Mansi", role: "Coordinator", image: "/team/Mansi.jpeg" },
-      { name: "Meghna Chauhan", role: "Coordinator", image: "/team/Meghna_Chauhan.jpg" },
+      { name: "Meghna Chauhan", role: "Coordinator", image: "/team/Meghna_Chauhan.jpeg" },
+      { name: "Neha Gupta", role: "Coordinator", image: "/team/Neha_Gupta.jpeg" },
       { name: "Rishika", role: "Coordinator", image: "/team/Rishika.jpeg" },
       { name: "Ritika Yadav", role: "Coordinator", image: "/team/Ritika_Yadav.jpeg" },
       { name: "Shivani Meena", role: "Coordinator", image: "/team/Shivani_Meena.jpg" },
       { name: "Shreya Pandey", role: "Coordinator", image: "/team/Shreya_Pandey.jpg" },
-      { name: "Sonakshi Jaiswal", role: "Coordinator", image: "/team/Sonakshi_Jaiswal.jpg" }
+      { name: "Sonakshi Jaiswal", role: "Coordinator", image: "/team/Sonakshi_Jaiswal.jpeg" }
     ].sort((a, b) => getRolePriority(a.role) - getRolePriority(b.role))
   },
   {
@@ -141,7 +143,8 @@ export const teamSections = [
     members: [
        { name: "Arya Chaturvedi", role: "Head Coordinator", image: "/team/Arya_Chaturvedi.jpg" },
        { name: "Rakshita", role: "Head Coordinator", image: "/team/Rakshita.jpg" },
-       { name: "Mishti Jain", role: "Coordinator", image: "/team/mishti.jpg" },
+       { name: "Mishti Jain", role: "Coordinator", image: "/team/Mishti_Jain.jpeg" },
+       { name: "Neha Gupta", role: "Coordinator", image: "/team/Neha_Gupta.jpeg" },
        { name: "Prachi Mann", role: "Coordinator", image: "/team/Prachi.jpeg" },
     ].sort((a, b) => getRolePriority(a.role) - getRolePriority(b.role))
   }
